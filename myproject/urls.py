3b4afd9768dfacfc7842755f1d2a0af53c9adbf8
Eve0317 <huang.xinya@northeastern.edu>
2023-12-04 13:53:08 -0800

    Signup_Login

--- myproject/urls.py
+++ myproject/urls.py
@@ -19,13 +19,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from myapp.views import index, contact, about
-
 urlpatterns = [
-    path("", index, name="index"),
-    path("items/", include("item.urls")),
-    path("contact/", contact, name="contact"),
-    path("about/", about, name="about"),
-
-    path("admin/", admin.site.urls),
+    path('',include('myapp.urls')),
+    path('items/', include("item.urls")),
+    path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
