7b12562044109e1efad8e3717a2afb35a0640d4a
Weizhou Zhou <zhou.weizh@northeastern.edu>
2023-12-05 19:29:08 -0800

    Clear errors from previous commits

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from item.models import item
+from item.models import Item
 
 @login_required
 def index(request):
